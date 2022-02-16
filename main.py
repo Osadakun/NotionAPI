cbf0c668acc5f5b1d37cb9828ed82abff2d6197c
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-17 00:30:28 +0900

    カレンダーの内容の取得に成功

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import requests
 import config
+from pprint import pprint
+import json
 
-url = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query"
+url = "https://api.notion.com/v1/databases/%s/query" %config.NOTION_DATABASE_ID
 headers = {
-  'Authorization': 'Bearer ' + NOTION_ACCESS_TOKEN,
-  'Notion-Version': '2021-05-13',
+  'Authorization': 'Bearer ' + config.NOTION_ACCESS_TOKEN,
+  'Notion-Version': '2021-08-16',
   'Content-Type': 'application/json',
 }
 r = requests.post(url, headers=headers)
+for i in range(len(r.json()["results"])+1):
+  name = r.json()['results'][i]['properties']['名前']['title'][0]['plain_text']
+  quantity = r.json()['results'][i]['properties']['日付']['date']['start']
+  quantity = quantity[:10]
+  print('%s: %s' %(name, quantity))
+# pprint(r.json(), sort_dicts=False)
