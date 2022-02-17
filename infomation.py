3fcedf6662fee92bbe155eb296820fc9c5dc9e92
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-17 23:21:49 +0900

    もう少しなような気がする

--- infomation.py
+++ infomation.py
@@ -4,7 +4,7 @@ import requests
 import config
 from pprint import pprint
 import json
-from datetime import datetime as dt
+from datetime import datetime
 
 url = "https://api.notion.com/v1/databases/%s/query" %config.NOTION_DATABASE_ID
 headers = {
@@ -13,17 +13,21 @@ headers = {
   'Content-Type': 'application/json',
 }
 r = requests.post(url, headers=headers)
+today_task = {}
 
 def notion():
   for i in range(len(r.json()["results"])):
     name = r.json()['results'][i]['properties']['名前']['title'][0]['plain_text']
     quantity = r.json()['results'][i]['properties']['日付']['date']['start']
     quantity = quantity[:10]
-    tdate = dt.strptime(quantity, "%Y-%m-%d")
-    return f"{name}:{tdate}"
+    tdate = datetime.strptime(quantity, "%Y-%m-%d")
+    today_task[tdate] = name
+  return today_task
 
 def w_txt():
   f = open("memo.txt", "w", encoding="UTF-8")
   inf = notion()
-  f.write(inf)
+  today_now = datetime.datetime.now()
+  x = today_task.get(today_now)
+  f.writelines(x)
   f.close()
