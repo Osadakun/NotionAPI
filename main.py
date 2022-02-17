d851b24300c88c8d4d166c36b2466d9535ae7462
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-17 15:26:06 +0900

    dateのフォーマットの修正

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
 import requests
 import config
 from pprint import pprint
 import json
+from datetime import datetime as dt
 
 url = "https://api.notion.com/v1/databases/%s/query" %config.NOTION_DATABASE_ID
 headers = {
@@ -10,9 +13,12 @@ headers = {
   'Content-Type': 'application/json',
 }
 r = requests.post(url, headers=headers)
-for i in range(len(r.json()["results"])+1):
+for i in range(len(r.json()["results"])):
   name = r.json()['results'][i]['properties']['名前']['title'][0]['plain_text']
   quantity = r.json()['results'][i]['properties']['日付']['date']['start']
   quantity = quantity[:10]
-  print('%s: %s' %(name, quantity))
+  tdate = dt.strptime(quantity, "%Y-%m-%d")
+  print(tdate)
+  # print(date(quantity))
+  # print('%s: %s' %(name, quantity))
 # pprint(r.json(), sort_dicts=False)
