5421b04387587ace2b9b779c83af9c6e65a361d3
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-03-11 15:49:36 +0900

    今から上手く複数項目でも表示する

--- information.py
+++ information.py
@@ -14,40 +14,36 @@ headers = {
 }
 r = requests.post(url, headers=headers)
 today_task = {}                   # タスクが入る，辞書型で管理
-
+tasks = ""
+tasks_number = 1
 
 def slicer(item):                 # 文字列変換＋スライス(時間で必要なのが前10個分だから)
   item = str(item)[:10]
   return item
 
-today_date = str(dt.now())
-today_date = slicer(today_date)
+today_now = str(dt.now())
+today_now = slicer(today_now)
 
 def notion():                     # Notionから情報を持ってくる
   for i in range(len(r.json()["results"])):
     name = r.json()['results'][i]['properties']['名前']['title'][0]['plain_text']
     quantity = r.json()['results'][i]['properties']['日付']['date']['start']
     quantity = slicer(quantity)      # 年月日だけ欲しいからスライス
-    tdate = dt.strptime(quantity, "%Y-%m-%d")
-    tdate = slicer(tdate)
-    if tdate == today_date:
-      today_task[name] = tdate
+    t_date = dt.strptime(quantity, "%Y-%m-%d")  # 取得した項目の日付
+    t_date = slicer(t_date)
+    if t_date == today_now:
+      today_task[tasks_number] = name
+      tasks_number += 1
   return today_task
-  # return today_task
-  # print(today_task)
-print(notion())
+
 def w_txt():
-  # f = open("memo.txt", "w", encoding="UTF-8")
-  inf = notion()
-  today_now = str(dt.now())       # datetime型ではスライス使えない
-  today_now = today_now[:10]      # 年月日だけ欲しいからスライス
-  today_now = dt.strptime(today_now, "%Y-%m-%d")
-  x = today_task.get(today_now)   # 今日にタスクあるかどうか確認
-  # f.write(x)
-  # f.close()
-  # print(inf)
-  # print(("--------------"))
-  # print(today_now)
-  # print(("--------------"))
-  # print(x)
-  # print(("--------------"))
+  inf = notion()                  # 今日のタスクが入る
+  inf_count = len(inf)            # 何個あるか調べる
+  if inf_count == 0:
+    return "今日のタスクはありません"
+  for i in range(1,inf_count+1):
+    if i == inf_count:
+      tasks += today_task + "です！"
+    else:
+      tasks += today_task[i] + "と"
+  return tasks
