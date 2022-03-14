5208602cfe6db7ea8ab713a9e77ef39152f87f74
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-03-14 15:37:58 +0900

    debug

--- scheduler.py
+++ scheduler.py
@@ -7,9 +7,8 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
+
 
 app = Flask(__name__)
 
